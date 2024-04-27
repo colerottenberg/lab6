de43e0521b689e583c0ba281b217c12a4677bdb1
Cole Rottenberg <cole.rottenberg@gmail.com>
2024-04-27 15:17:09 -0400

    adding alu files

---
+++ alu_tb.vhd
@@ -0,0 +1,21 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+use std.env.finish;
+
+library osvvm;
+use osvvm.RandomPkg.all;
+use osvvm.CoveragePkg.all;
+
+use work.DataStructures.all;
+
+-- enter your code below
+entity alu_tb is 
+
+end alu_tb;
+
+architecture tb of alu_tb is 
+begin 
+
+end tb;
